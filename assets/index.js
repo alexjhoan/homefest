6e49734c3dde5985a423a08021d4dde8996a0a2f
avivas89 <avivaspati@gmail.com>
2022-06-02 17:13:36 -0400

    new version homefest

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,19 @@
 $(window).on('load', function () {
   $('body').css('opacity', '1');
+
+     $(".linkTo").click(function (e) {
+    e.preventDefault()
+    $("header .collapse.show").removeClass("show")
+    const url = $(this).attr("href")
+    const header = $("header").height()
+    if (!url.includes("html")) {
+      const section = $(url.slice(1)).offset().top;
+      window.scrollTo({top: section - header,behavior: "smooth"});
+    } else{
+      window.location = url
+    }
+  })
+
 });
 
 let offset
@@ -14,3 +28,111 @@ new WOW({offset:offset, scrollContainer: null}).init()
 
 $('header').load('components/header.html')
 $('footer').load('components/footer.html')
+
+// ------------------------------Banner-----------------------------
+const swiperbanner = new Swiper(".swiper-banner", {
+  spaceBetween: 0,
+  loop: true,
+  slidesPerView: 1,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  on: {
+    slideChangeTransitionStart: function () {
+      $('.banner_title').hide(0);
+      $('.banner_title').removeClass('animated');
+    },
+    slideChangeTransitionEnd: function () {
+      $('.swiper-slide-active .banner_title').show(0).addClass('animated');
+    },
+  }
+});
+
+// ------------------------------Evento Areas-----------------------------
+$('.areas .areas_grid .item').each(function(){
+  $(this).click(function(){
+    $(this).find('ul').toggleClass('list-active')
+    $(this).siblings().find('ul').removeClass('list-active')
+  })
+});
+
+
+// ------------------------------Actividades-----------------------------
+if (screen.width < 768) {
+    $(".gallery").addClass("swiper")
+    $(".wrapper").addClass("swiper-wrapper")
+    $(".gallery .wrapper .col").addClass("swiper-slide")
+      const mygallery = new Swiper(".gallery", {
+        spaceBetween: 9,
+        loop: true,
+        slidesPerView: 1,
+        // autoplay: {
+        //   delay: 3000,
+        //   disableOnInteraction: false,
+        // },
+        breakpoints:{
+          0:{
+            slidesPerView: 1
+          },
+          574:{
+            slidesPerView: 2
+          }
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      })
+  }
+
+
+//-- ------------------------------Forms--------------------------------
+
+function sendForm(formId) {
+  'use strict'
+  const form = document.querySelector(`#${formId}`)
+  const data = {
+    nombre: form.firstname.value,
+    email: form.email.value,
+    telefono: form.phone.value,
+    cantidad: formId == "contactForm" ? "" : form.cantidad.value ,
+    source: 2,
+    utm_source: "web_cliente",
+    utm_medium: "home_fest",
+    InfoLeads: 1,
+    IDflow_execution: 4315
+  }
+  if (!form.checkValidity()) {
+    event.preventDefault()
+    event.stopPropagation()
+  }else{
+    fetch(`https://www.infocasas.com.uy/gracias/lanzamiento-home-fest?nombre=${data.nombre}&email=${data.email}&telefono=${data.telefono}&utm_source=web_cliente&utm_medium=home_fest`)
+    .then((json) => {
+      setTimeout(()=>{
+        if (json.status === 200) {
+          $(form).siblings('#formSuccess').fadeIn();
+        } else {
+          $(form).siblings('#formError').fadeIn();
+        }
+        $(form).siblings('#formSending').hide();
+      }, 2000)
+    })
+    .catch(error => {
+      console.log('error', error);
+      setTimeout(() => {
+        $(form).siblings('#formSending').hide();
+        $(form).siblings('#formError').fadeIn();
+      }, 2000)
+    });
+    setTimeout(()=>{
+      $(form).fadeOut();
+      $(form).siblings('#formSending').fadeIn();
+    },300)
+  }
+  form.classList.add('was-validated')
+}
